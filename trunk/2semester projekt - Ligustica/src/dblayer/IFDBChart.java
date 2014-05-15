1ae10bc3a96b1d3b5f26e440f90450af6173c489
ThomasWintherBonderup@gmail.com <ThomasWintherBonderup@gmail.com@1a9d04d4-bb8e-49fd-6480-9ad32f21c3d9>
2014-05-15 08:13:03 +0000

    IFDBChart with methods

--- trunk/2semester projekt - Ligustica/src/dblayer/IFDBChart.java
+++ trunk/2semester projekt - Ligustica/src/dblayer/IFDBChart.java
@@ -1,10 +1,26 @@
 package dblayer;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modellayer.Chart;
+
 /**
  * 
  * @author Gruppe 3
  *
  */
-public class IFDBChart {
+public interface IFDBChart {
+	public ArrayList<Chart> getAllCharts(boolean retriveAssociation); 
 
+	public Chart searchChartOnName(String name, boolean retriveAssociation) throws SQLException;
+
+	public Chart selectSingleChart(int chartID, boolean retriveAssociation) throws SQLException;
+
+	public int insertChart(Chart c) throws SQLException; 
+
+	public int updateChart(Chart c) throws SQLException; 
+	
+	public int deleteChart(Chart c) throws SQLException;
+  
 }
